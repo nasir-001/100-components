a21f58e684c3dc4e114364f33defd082ed216945
Nasir <nasirlawal001@gmail.com>
2021-04-17 10:27:13 +0100

    add custom forms and github pages

--- src/views/Layout2.vue
+++ src/views/Layout2.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    This is the second page
+  <div class="flex justify-center shadow-md mx-auto mt-20 bg-white w-4/6 border-4 rounded-xl border-gray-800">
+  <div class="mt-16">
+    <p class="text-center font-semibold text-lg">Search for statistics</p>
+  </div>
   </div>
 </template>
 
-<script>
+<script type="application/javascript">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
