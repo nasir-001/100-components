a4b648cee01b94570221a8a7d8f5398872e875d4
Nasir <nasirlawal001@gmail.com>
2021-04-17 19:46:24 +0100

    add customization to the colors

--- src/views/Layout1.vue
+++ src/views/Layout1.vue
@@ -22,7 +22,7 @@
           <div class="bg-white mx-auto w-12 h-12 rounded-full mt-8">
             <p class="text-white">.</p>
             <svg class="flex justify-center h-7 w-7 mx-2 -mt-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-              viewBox="0 0 192.904 192.904" style="enable-background:new 0 0 192.904 192.904;" xml:space="preserve">
+              viewBox="0 0 192.904 192.904" xml:space="preserve">
             <path d="M190.707,180.101l-47.078-47.077c11.702-14.072,18.752-32.142,18.752-51.831C162.381,36.423,125.959,0,81.191,0
               C36.422,0,0,36.423,0,81.193c0,44.767,36.422,81.187,81.191,81.187c19.688,0,37.759-7.049,51.831-18.751l47.079,47.078
               c1.464,1.465,3.384,2.197,5.303,2.197c1.919,0,3.839-0.732,5.304-2.197C193.637,187.778,193.637,183.03,190.707,180.101z M15,81.193
