e5431eb581b076f3b276575ce4128d9998ef352c
Nasir <nasirlawal001@gmail.com>
2021-04-23 17:09:53 +0100

    finishing layout 10 completely

--- src/views/Layout10.vue
+++ src/views/Layout10.vue
@@ -49,26 +49,42 @@
                 Starting at $8
               </div>
             </div>
-            <div class="ml-6 text-xs mt-5 text-gray-500 font-semibold underline">
+            <div class="ml-6 text-xs cursor-pointer mt-5 text-gray-500 font-semibold underline">
               Learn more about licenses
             </div>
           </div>
-          <div class="py-3 rounded-md bg-layout10secondColor mt-5 text-white text-center">
+          <div class="py-3 rounded-md bg-layout10secondColor cursor-pointer mt-5 text-white text-center">
             Add to cart
           </div>
-          <div class="py-2 rounded-md text-layout10secondColor mt-3 border-2 border-gray-200 bg-white text-center">
+          <div class="py-2 rounded-md text-layout10secondColor cursor-pointer mt-3 border-2 border-gray-200 bg-white text-center">
             Add to cart
           </div>
         </div>
       </div>
       <div class="">
-        <div class="bg-white w-96 rounded-xl h-72 -ml-10 -mt-20 shadow-2xl">
-          
+        <div class="bg-white w-96 rounded-xl h-72 -ml-14 -mt-24 shadow-2xl">
+          <div class="pt-10">
+            <img class="rounded-full mx-auto w-14 h-14 object-cover" src="/img/pexels-luriko-yamaguchi-2804282.jpg" alt="" srcset="">
+          </div>
+          <div class="font-semibold text-center mt-1 text-gray-700 text-lg">
+            Etta Lamb
+          </div>
+          <div class="mt-4 text-gray-400 text-sm font-semibold text-center mx-10">
+            For custom orders please contact us at design@email.com
+          </div>
+          <div class="flex justify-center mx-16 mt-6">
+            <div class="py-2 px-2 cursor-pointer rounded-md border-2 border-gray-100 mx-2 w-1/2 text-center text-layout10secondColor">
+              Follow
+            </div>
+            <div class="py-2 px-2 cursor-pointer rounded-md mx-2 w-1/2 text-center text-white bg-layout10secondColor">
+              Message
+            </div>
+          </div>
         </div>
         <div class="w-108 bg-white rounded-xl shadow-2xl mt-20 h-28 ml-8">
           <div class="flex justify-center">
             <div class="border-2 border-gray-100 mx-1 rounded mt-10 w-24">
-              <div class="flex py-1.5 justify-between text-gray-500">
+              <div class="flex py-1.5 justify-between text-gray-500 cursor-pointer">
                 <div class="w-4 h-4 mx-2">
                   <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.67 64"><path class="cls-1" d="M40,42.67a10.74,10.74,0,0,0-7.6,3.17L20,37.15a10.64,10.64,0,0,0,0-10.3l12.4-8.69a10.67,10.67,0,1,0-3.07-7.49,10.6,10.6,0,0,0,.4,2.88L16.29,23a10.41,10.41,0,0,0-5.62-1.63,10.67,10.67,0,0,0,0,21.34A10.41,10.41,0,0,0,16.29,41l13.44,9.41A10.67,10.67,0,1,0,40,42.67ZM40,5.33A5.34,5.34,0,0,1,40,16a5.24,5.24,0,0,1-3.2-1.09A5.32,5.32,0,0,1,40,5.33ZM15.57,34.05A5.27,5.27,0,0,1,11,37.31a1.32,1.32,0,0,1-.29,0,5.33,5.33,0,1,1,0-10.66,1.32,1.32,0,0,1,.29,0A5.27,5.27,0,0,1,15.57,30a5.1,5.1,0,0,1,0,4.1ZM40,58.67a5.32,5.32,0,0,1-3.2-9.58A5.24,5.24,0,0,1,40,48a5.34,5.34,0,0,1,0,10.67Z"/></svg>
                 </div>
@@ -78,7 +94,7 @@
               </div>
             </div>
             <div class="border-2 border-gray-100 mx-1 rounded mt-10 w-32">
-              <div class="flex py-1.5 justify-between text-gray-500">
+              <div class="flex py-1.5 justify-between text-gray-500 cursor-pointer">
                 <div class="w-4 h-4 mx-2">
                   <svg class="w-7 h-7 -mt-1 fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
                     <path d="M23.024,39.274c0.189,0.212,0.461,0.333,0.745,0.333h0.001c0.284,0,0.556-0.122,0.745-0.334l13.184-14.771
@@ -95,7 +111,7 @@
               </div>
             </div>
             <div class="border-2 border-gray-100 mx-1 rounded mt-10 w-32">
-              <div class="flex py-1.5 justify-between text-gray-500">
+              <div class="flex py-1.5 justify-between text-gray-700 cursor-pointer">
                 <div class="w-4 h-4 mx-2">
                   <svg class="mt-0.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400 0 512z"/></svg>
                 </div>
