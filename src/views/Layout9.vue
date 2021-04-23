a9cdb63cd235c4587f34b4699f7a42aeecae5045
Nasir <nasirlawal001@gmail.com>
2021-04-23 08:48:03 +0100

    finish styling component 9

--- src/views/Layout9.vue
+++ src/views/Layout9.vue
@@ -6,14 +6,17 @@
           <div class="font-semibold ml-8 text-lg">
             Nearby friends
           </div>
-          <div class="text-sm font-semibold underline mr-8">
+          <div class="text-sm font-semibold cursor-pointer underline mr-8">
             See all
           </div>
         </div>
         <div class="flex mx-8 mt-6">
           <div class="w-1/4">
             <div class="mx-0.5 h-36 rounded-lg border-2 border-layout9cardBorderColor">
-              first
+              <img class="w-14 h-14 mt-4 mx-8 object-cover rounded-full" src="/img/pexels-ariel-prajatama-4124367.jpg" alt="" srcset="">
+              <div class="text-center mt-0.5 text-lg font-medium">
+                Josh
+              </div>
             </div>
             <div class="bg-black mx-auto rounded-full w-8 h-8 -mt-5">
               <div class="text-white text-center text-3xl font-thin">
@@ -27,7 +30,10 @@
           </div>
           <div class="w-1/4">
             <div class="mx-0.5 h-36 rounded-lg border-2 border-layout9cardBorderColor">
-              first
+              <img class="w-14 h-14 mt-4 mx-8 object-cover rounded-full" src="/img/pexels-creative-hussain-2774197.jpg" alt="" srcset="">
+              <div class="text-center mt-0.5 text-lg font-medium">
+                Christer
+              </div>
             </div>
             <div class="bg-black mx-auto rounded-full w-8 h-8 -mt-5">
               <div class="text-white text-center text-3xl font-thin">
@@ -41,7 +47,10 @@
           </div>
           <div class="w-1/4">
             <div class="mx-0.5 h-36 rounded-lg border-2 border-layout9cardBorderColor">
-              first
+              <img class="w-14 h-14 mt-4 mx-8 object-cover rounded-full" src="/img/pexels-justin-shaifer-1222271.jpg" alt="" srcset="">
+              <div class="text-center mt-0.5 text-lg font-medium">
+                Lise
+              </div>
             </div>
             <div class="bg-black mx-auto rounded-full w-8 h-8 -mt-5">
               <div class="text-white text-center text-3xl font-thin">
@@ -55,7 +64,10 @@
           </div>
           <div class="w-1/4">
             <div class="mx-0.5 h-36 rounded-lg border-2 border-layout9cardBorderColor">
-              first
+              <img class="w-14 h-14 mt-4 mx-8 object-cover rounded-full" src="/img/pexels-luriko-yamaguchi-2804282.jpg" alt="" srcset="">
+              <div class="text-center mt-0.5 text-lg font-medium">
+                Katrine
+              </div>
             </div>
             <div class="bg-black mx-auto rounded-full w-8 h-8 -mt-5">
               <div class="text-white text-center text-3xl font-thin">
@@ -69,17 +81,106 @@
           </div>
         </div>
       </div>
-      <div class="bg-white mx-auto -ml-48 mt-24 shadow-2xl h-124 rounded-lg w-4/12">
-        second
+      <div class="bg-white mx-auto -ml-44 mt-24 shadow-2xl h-124 rounded-lg w-2/7">
+        <div class="mx-auto mt-14 flex justify-start">
+          <div class="font-semibold ml-10 text-lg">
+            Contacts 
+          </div>
+          <div class="font-semibold ml-2 text-lg border border-black rounded-2xl px-3">
+            43
+          </div>
+        </div>
+        <div class="flex justify-between mx-8 mt-6">
+          <div class="flex justify-start">
+            <div class="bg-white">
+              <img class="w-14 h-14 object-cover rounded-full" src="/img/pexels-justin-shaifer-1222271.jpg" alt="" srcset="">              
+            </div>
+            <div class="bg-white ml-4 mt-0.5">
+              <div class="font-semibold text-lg">
+                Lise
+              </div>
+              <div class="-mt-1 text-sm font-semibold text-gray-700">
+                Bergen, Norway
+              </div>
+            </div>
+          </div>
+          <div class="mt-3">
+            <svg class="w-6 h-6 font-thin fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,7H7A1,1,0,0,0,7,9H17a1,1,0,0,0,0-2Zm0,4H7a1,1,0,0,0,0,2H17a1,1,0,0,0,0-2Zm2-9H5A3,3,0,0,0,2,5V15a3,3,0,0,0,3,3H16.59l3.7,3.71A1,1,0,0,0,21,22a.84.84,0,0,0,.38-.08A1,1,0,0,0,22,21V5A3,3,0,0,0,19,2Zm1,16.59-2.29-2.3A1,1,0,0,0,17,16H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4H19a1,1,0,0,1,1,1Z"/></svg>
+          </div>
+        </div>
+        <div class="flex justify-between mx-8 mt-6">
+          <div class="flex justify-start">
+            <div class="bg-white">
+              <img class="w-14 h-14 object-cover rounded-full" src="/img/pexels-mostafa-sannad-868113.jpg" alt="" srcset="">              
+            </div>
+            <div class="bg-white ml-4 mt-0.5">
+              <div class="font-semibold text-lg">
+                Emily
+              </div>
+              <div class="-mt-1 text-sm font-semibold text-gray-700">
+                Bergen, Norway
+              </div>
+            </div>
+          </div>
+          <div class="mt-3">
+            <svg class="w-6 h-6 font-thin fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,7H7A1,1,0,0,0,7,9H17a1,1,0,0,0,0-2Zm0,4H7a1,1,0,0,0,0,2H17a1,1,0,0,0,0-2Zm2-9H5A3,3,0,0,0,2,5V15a3,3,0,0,0,3,3H16.59l3.7,3.71A1,1,0,0,0,21,22a.84.84,0,0,0,.38-.08A1,1,0,0,0,22,21V5A3,3,0,0,0,19,2Zm1,16.59-2.29-2.3A1,1,0,0,0,17,16H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4H19a1,1,0,0,1,1,1Z"/></svg>
+          </div>
+        </div>
+        <div class="flex justify-between mx-8 mt-6">
+          <div class="flex justify-start">
+            <div class="bg-white">
+              <img class="w-14 h-14 object-cover rounded-full" src="/img/pexels-polina-tankilevitch-4728658.jpg" alt="" srcset="">              
+            </div>
+            <div class="bg-white ml-4 mt-0.5">
+              <div class="font-semibold text-lg">
+                Jake
+              </div>
+              <div class="-mt-1 text-sm font-semibold text-gray-700">
+                Bergen, Norway
+              </div>
+            </div>
+          </div>
+          <div class="mt-3">
+            <svg class="w-6 h-6 font-thin fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,7H7A1,1,0,0,0,7,9H17a1,1,0,0,0,0-2Zm0,4H7a1,1,0,0,0,0,2H17a1,1,0,0,0,0-2Zm2-9H5A3,3,0,0,0,2,5V15a3,3,0,0,0,3,3H16.59l3.7,3.71A1,1,0,0,0,21,22a.84.84,0,0,0,.38-.08A1,1,0,0,0,22,21V5A3,3,0,0,0,19,2Zm1,16.59-2.29-2.3A1,1,0,0,0,17,16H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4H19a1,1,0,0,1,1,1Z"/></svg>
+          </div>
+        </div>
+        <div class="flex justify-between mx-8 mt-6">
+          <div class="flex justify-start">
+            <div class="bg-white">
+              <img class="w-14 h-14 object-cover rounded-full" src="/img/pexels-luriko-yamaguchi-2804282.jpg" alt="" srcset="">              
+            </div>
+            <div class="bg-white ml-4 mt-0.5">
+              <div class="font-semibold text-lg">
+                Cheryl
+              </div>
+              <div class="-mt-1 text-sm font-semibold text-gray-700">
+                Bergen, Norway
+              </div>
+            </div>
+          </div>
+          <div class="mt-3">
+            <svg class="w-6 h-6 font-thin fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,7H7A1,1,0,0,0,7,9H17a1,1,0,0,0,0-2Zm0,4H7a1,1,0,0,0,0,2H17a1,1,0,0,0,0-2Zm2-9H5A3,3,0,0,0,2,5V15a3,3,0,0,0,3,3H16.59l3.7,3.71A1,1,0,0,0,21,22a.84.84,0,0,0,.38-.08A1,1,0,0,0,22,21V5A3,3,0,0,0,19,2Zm1,16.59-2.29-2.3A1,1,0,0,0,17,16H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4H19a1,1,0,0,1,1,1Z"/></svg>
+          </div>
+        </div>
+        <div class="px-3 py-2 mt-8 mx-8 bg-black w-32 rounded-full text-sm text-white text-center font-semibold">
+          View everyone
+        </div>
       </div>
+      <page-link class="mr-10">
+        <a href="https://www.uidesigndaily.com/posts/figma-people-list-card-day-1310" target="_blank" rel="noopener noreferrer">page link</a>
+      </page-link>
     </div>
   </div>
 </template>
 
 <script type="application/javascript">
 import { defineComponent } from 'vue';
+import PageLink from 'vue';
 
 export default defineComponent({
-  name: 'Layout'
+  name: 'Layout',
+  components: {
+    PageLink
+  }
 })
 </script>
