d6a6d2caee57fca8949caa1323def2ce17e053d6
Nasir <nasirlawal001@gmail.com>
2021-04-26 15:03:05 +0100

    finish positioning component 12 cards

--- src/views/Layout12.vue
+++ src/views/Layout12.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    Layout 12
+  <div class="mt-20">
+    <div class="bg-white shadow-2xl rounded-md bg-opacity-80 w-3/6 ml-52 h-108">
+      first
+    </div>
+    <div class="flex justify-center mb-20">
+      <div class="bg-white rounded-md h-96 -mt-10 -ml-32 bg-opacity-80 shadow-2xl w-3/7">
+        second
+      </div>
+      <div class="bg-white rounded-md w-96 -mt-24 bg-opacity-80 ml-20 h-108">
+        third
+      </div>
+    </div>
   </div>
 </template>
 
