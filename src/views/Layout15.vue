851a92d6d0bb08a6aa8f315506ed5ffbdb33f4de
Nasir <nasirlawal001@gmail.com>
2021-04-28 10:33:37 +0100

    finishing component 15

--- src/views/Layout15.vue
+++ src/views/Layout15.vue
@@ -2,7 +2,7 @@
   <div class="bg-layout15backgroundColor h-screen">
     <div class="flex justify-center top-20 sticky">
       <div class="w-144 mx-10">
-        <div class="bg- w-full bg-white rounded-xl h-32">
+        <div class="bg- w-full bg-white rounded-xl h-32 shadow-xl">
           <div class="mx-8">
             <div class="flex justify-between">
               <div class="pt-6">
@@ -22,7 +22,7 @@
               </div>
               <div class="bg-layout15textAndButtonColor mt-9 w-36 rounded-full shadow-xl">
                 <div class="flex justify-between">
-                  <div class="text-gray-100 text-xs mt-3 ml-5 font-semibold">
+                  <div class="text-gray-100 text-xs mt-3 ml-5 font-bold">
                     View role
                   </div>
                   <div class="w-8 h-8 text-center text-white rounded-full bg-layout15arrorCircleColor mt-1.5 mr-2">
@@ -33,7 +33,7 @@
             </div>
           </div>
         </div>
-        <div class="bg- w-full mt-4 bg-white rounded-xl h-32">
+        <div class="bg- w-full mt-4 bg-white rounded-xl h-32 shadow-xl">
           <div class="mx-8">
             <div class="flex justify-between">
               <div class="pt-6">
@@ -51,12 +51,43 @@
                   </div>
                 </div>
               </div>
-              <div class="bg-layout15textAndButtonColor mt-9 w-36 rounded-full shadow-xl">
+              <div class="mt-9 w-36 rounded-full border-2 border-layout15textAndButtonColor">
                 <div class="flex justify-between">
-                  <div class="text-gray-100 text-xs mt-3 ml-5 font-semibold">
+                  <div class="text-layout15textAndButtonColor text-xs mt-3 ml-5 font-bold">
                     View role
                   </div>
-                  <div class="w-8 h-8 text-center text-white rounded-full bg-layout15arrorCircleColor mt-1.5 mr-2">
+                  <div class="w-8 h-8 text-center text-layout15textAndButtonColor mt-1.5 mr-2">
+                    <svg class="fill-current mt-2 ml-1 h-4 w-6" enable-background="new 0 0 32 32"  id="Слой_1" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path clip-rule="evenodd" d="M32,16.009c0-0.267-0.11-0.522-0.293-0.714  l-9.899-9.999c-0.391-0.395-1.024-0.394-1.414,0c-0.391,0.394-0.391,1.034,0,1.428l8.193,8.275H1c-0.552,0-1,0.452-1,1.01  s0.448,1.01,1,1.01h27.586l-8.192,8.275c-0.391,0.394-0.39,1.034,0,1.428c0.391,0.394,1.024,0.394,1.414,0l9.899-9.999  C31.894,16.534,31.997,16.274,32,16.009z" fill-rule="evenodd" id="Arrow_Forward"/><g/><g/><g/><g/><g/><g/></svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg- w-full mt-4 bg-white rounded-xl h-32 shadow-xl">
+          <div class="mx-8">
+            <div class="flex justify-between">
+              <div class="pt-6">
+                <div class="text-base font-bold tracking-normal text-layout15textAndButtonColor">
+                  Product Designer
+                </div>
+                <div>
+                  <div class="flex justify-start">
+                    <div class="bg-layout15badgeColor border-2 border-gray-100 mt-2 w-16 py-0.5 mr-1 text-center font-semibold text-xs text-layout15textAndButtonColor rounded-full">
+                      Product
+                    </div>
+                    <div class="bg-layout15badgeColor border-2 border-gray-100 mt-2 w-24 py-0.5 text-center font-semibold text-xs text-layout15textAndButtonColor rounded-full">
+                      Remote role
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-9 w-36 rounded-full border-2 border-layout15textAndButtonColor">
+                <div class="flex justify-between">
+                  <div class="text-layout15textAndButtonColor text-xs mt-3 ml-5 font-bold">
+                    View role
+                  </div>
+                  <div class="w-8 h-8 text-center text-layout15textAndButtonColor mt-1.5 mr-2">
                     <svg class="fill-current mt-2 ml-1 h-4 w-6" enable-background="new 0 0 32 32"  id="Слой_1" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path clip-rule="evenodd" d="M32,16.009c0-0.267-0.11-0.522-0.293-0.714  l-9.899-9.999c-0.391-0.395-1.024-0.394-1.414,0c-0.391,0.394-0.391,1.034,0,1.428l8.193,8.275H1c-0.552,0-1,0.452-1,1.01  s0.448,1.01,1,1.01h27.586l-8.192,8.275c-0.391,0.394-0.39,1.034,0,1.428c0.391,0.394,1.024,0.394,1.414,0l9.899-9.999  C31.894,16.534,31.997,16.274,32,16.009z" fill-rule="evenodd" id="Arrow_Forward"/><g/><g/><g/><g/><g/><g/></svg>
                   </div>
                 </div>
@@ -65,7 +96,7 @@
           </div>
         </div>
       </div>
-      <div class="w-80 bg-white -mt-14 rounded-xl h-72">
+      <div class="w-80 bg-white -mt-14 rounded-xl h-72 shadow-2xl">
         <div class="mt-8 mx-8">
           <div class="flex justify-between">
             <div class="w-4/5 text-layout15textAndButtonColor font-medium text-base">
