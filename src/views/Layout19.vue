b7e5af19022198c0845ac54d19b46d8c11833f9a
Nasir <nasirlawal001@gmail.com>
2021-04-30 15:28:04 +0100

    create component 19 layout

--- src/views/Layout19.vue
+++ src/views/Layout19.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="bg-layout19backgroundColor h-screen">
-    Layout 19
+    <div class="w-164 mx-auto rounded-lg h-108 bg-white top-20 sticky">
+      <div class="bg-layout19backgroundColor text-center mt-6 mr-4 w-8 h-8 rounded-full float-right text-lg font-thin">
+        X
+      </div>
+      <div class="flex justify-between pt-32 mx-12">
+        
+      </div>
+    </div>
   </div>
 </template>
 
