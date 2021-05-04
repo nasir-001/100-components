6ae00808bda21810f34104d1b0001fb6e692d7c0
Nasir <nasirlawal001@gmail.com>
2021-05-05 00:00:34 +0100

    add cursor-pointer to the view button in components list

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@
       <div class="text-gray-500 uppercase py-4 font-semibold">
         Component 1
       </div>
-      <div class="border border-gray-400 text-gray-500 text-center h-10 mt-2 pt-1 uppercase px-3 rounded-full w-24 font-semibold">
+      <div class="border border-gray-400 text-gray-500 text-center h-10 mt-2 pt-1 uppercase px-3 rounded-full w-24 font-semibold cursor-pointer">
         View
       </div>
     </div>
