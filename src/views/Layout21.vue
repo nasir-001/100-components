03c2af4c3742cc4fdce4a88c6449492eee93e7f3
Nasir <nasirlawal001@gmail.com>
2021-05-05 15:28:03 +0100

    add two cards to the component 21

--- src/views/Layout21.vue
+++ src/views/Layout21.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="bg-layout21backgroundColor h-screen">
-    Layout 21
+    <div class="flex justify-center top-20 sticky">
+      <div class="bg-white rounded-2xl mx-10 w-72 h-48">
+        card
+      </div>
+      <div class="bg-white rounded-2xl mx-10 w-72 h-48">
+        card
+      </div>
+    </div>
   </div>
 </template>
 
