849379d2da5fe5770e44957ae3f4158e7f9d4cef
Nasir <nasirlawal001@gmail.com>
2021-05-06 12:04:12 +0100

    add the second mark flex and circle

--- src/views/Layout22.vue
+++ src/views/Layout22.vue
@@ -40,6 +40,15 @@
           Included source files
         </div>
       </div>
+      <div class="flex justify-start mx-28">
+        <div class="bg-layout22smallCircleColor text-layout22backgroundColor w-7 h-7 rounded-full mt-6">
+          <svg class="fill-current w-6 h-6 mt-0.5 ml-0.5" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+          <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+            c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+            "/>
+          </svg>
+        </div>
+      </div>
     </div>
   </div>
 </template>
