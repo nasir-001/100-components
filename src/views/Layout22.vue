f307ae1ffaea3af2400527e6803a55511887cf91
Nasir <nasirlawal001@gmail.com>
2021-05-06 12:09:11 +0100

    add shadow to the card in component 22

--- src/views/Layout22.vue
+++ src/views/Layout22.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-layout22backgroundColor h-screen">
-    <div class="bg-white w-124 h-130 rounded-xl top-6 sticky mx-auto">
+    <div class="bg-white w-124 h-130 rounded-xl top-6 sticky mx-auto shadow-2xl">
       <div class="border-2 border-gray-100 rounded-xl w-5/6 h-20 mx-auto top-16 sticky">
         <div class="flex justify-evenly mx-8">
           <div class="text-sm font-semibold pt-6 cursor-pointer">
