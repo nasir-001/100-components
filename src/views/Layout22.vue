8f69dca8a23912867d2422728d3312ecb7361442
Nasir <nasirlawal001@gmail.com>
2021-05-06 11:13:53 +0100

    add the first flex item

--- src/views/Layout22.vue
+++ src/views/Layout22.vue
@@ -2,7 +2,12 @@
   <div class="bg-layout22backgroundColor h-screen">
     <div class="bg-white w-124 h-130 rounded-xl top-6 sticky mx-auto">
       <div class="border-2 border-gray-100 rounded-xl w-5/6 h-20 mx-auto top-16 sticky">
-        flex
+        <div class="flex justify-evenly mx-12">
+          <div class="text-sm font-semibold pt-6">
+            Basic
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
