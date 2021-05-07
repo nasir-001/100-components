2074dd1a1190e8c2c5c5caf37356894ba3c5c35b
Nasir <nasirlawal001@gmail.com>
2021-05-07 18:41:36 +0100

    remove router-link and replace it with slot component

--- src/components/SingleComponent.vue
+++ src/components/SingleComponent.vue
@@ -22,11 +22,7 @@
         Component 1
       </div>
     </div>
-    <router-link class="border border-gray-400 text-gray-500 text-center h-10 mt-2 pt-2 px-3 rounded-full w-24 font-semibold cursor-pointer hover:bg-gray-200 hover:bg-opacity-50" to="/layout1" exact>
-      <div class="animate-bounce">
-        View
-      </div>
-    </router-link>
+    <slot></slot>
   </div>
 </template>
 
