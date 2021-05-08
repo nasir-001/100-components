3c1df114931a47e71ea2218183ce724dc3c3baef
Nasir <nasirlawal001@gmail.com>
2021-05-08 13:52:17 +0100

    move component list to the SingleComponent component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="my-20 w-4/5 mx-auto">
+    <single-component>
+      <router-link class="border border-gray-400 text-gray-500 text-center h-10 mt-2 pt-2 px-3 rounded-full w-24 font-semibold cursor-pointer hover:bg-gray-200 hover:bg-opacity-50" to="/layout1" exact>
+        <div class="animate-bounce">
+          View
+        </div>
+      </router-link>
+    </single-component>
     <div class="flex justify-between border-b-2 px-4 hover:shadow-2xl hover:bg-gray-50 rounded-lg border-gray-300 mb-3">
       <div class="flex justify-start">
         <div class="text-gray-600 mt-4 mr-2">
@@ -644,8 +651,12 @@
 
 <script>
 import { defineComponent } from 'vue';
+import SingleComponent from '../components/SingleComponent.vue';
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  components: {
+    SingleComponent
+  }
 })
 </script>
