666477e34d6a5b5c0788cc53ff81be75b62ea3b9
Nasir <nasirlawal001@gmail.com>
2021-05-09 16:18:30 +0100

    add cursor pointer to the component 9

--- src/views/Layout9.vue
+++ src/views/Layout9.vue
@@ -162,7 +162,7 @@
             <svg class="w-6 h-6 font-thin fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,7H7A1,1,0,0,0,7,9H17a1,1,0,0,0,0-2Zm0,4H7a1,1,0,0,0,0,2H17a1,1,0,0,0,0-2Zm2-9H5A3,3,0,0,0,2,5V15a3,3,0,0,0,3,3H16.59l3.7,3.71A1,1,0,0,0,21,22a.84.84,0,0,0,.38-.08A1,1,0,0,0,22,21V5A3,3,0,0,0,19,2Zm1,16.59-2.29-2.3A1,1,0,0,0,17,16H5a1,1,0,0,1-1-1V5A1,1,0,0,1,5,4H19a1,1,0,0,1,1,1Z"/></svg>
           </div>
         </div>
-        <div class="px-3 py-2 mt-8 mx-8 bg-black w-32 rounded-full text-sm text-white text-center font-semibold">
+        <div class="px-3 py-2 mt-8 cursor-pointer mx-8 bg-black w-32 rounded-full text-sm text-white text-center font-semibold">
           View everyone
         </div>
       </div>
