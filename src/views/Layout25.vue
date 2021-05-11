d982572c25b7d2baab8d771e2873f27346831cd6
Nasir <nasirlawal001@gmail.com>
2021-05-11 14:32:30 +0100

    add the last flex to the first card

--- src/views/Layout25.vue
+++ src/views/Layout25.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-layout25backgroundColor h-screen">
     <div class="flex justify-center top-10 sticky">
-      <div class="bg-white w-80 h-60 mx-10 rounded-xl">
+      <div class="bg-white w-80 h-64 mx-10 rounded-xl">
         <div class="pt-8 ml-8 mr-24 font-semibold text-lg">
           Choose a view for the map
         </div>
@@ -12,7 +12,18 @@
           <div class="font-semibold text-sm mt-1">
             Daily Availability
             <div class="text-xs text-gray-600 mt-1">
-              View Hours per day a room is open
+              View the hours per day a room is open
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-start mx-8 mt-4">
+          <div class="mr-2">
+            <input class="w-5 h-5 text-black" type="radio">
+          </div>
+          <div class="font-semibold text-sm mt-1">
+            Weekly Capacity
+            <div class="text-xs text-gray-600 mt-1">
+              View the hours per week a room is open
             </div>
           </div>
         </div>
