cadd717523bf961d26bb128827d6f850dc464da8
Nasir <nasirlawal001@gmail.com>
2021-05-11 12:57:50 +0100

    reduce the margin top and bottom from the component

--- src/views/Layout24.vue
+++ src/views/Layout24.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-component24backgroundColor h-screen">
     <div class="bg-white w-4/6 mx-auto h-144 rounded-2xl top-10 sticky">
-      <div class="text-center text-3xl font-semibold font-serif pt-14">
+      <div class="text-center text-3xl font-semibold font-serif pt-10">
         Help Center
       </div>
-      <div class="relative mx-auto mt-10 bg-white w-3/6 mb-10">
+      <div class="relative mx-auto mt-8 bg-white w-3/6 mb-8">
         <div class="absolute inset-y-0 left-0 text-gray-400 flex items-center pl-3">
           <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current">
             <path d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path>
