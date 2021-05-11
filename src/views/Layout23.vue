2b2be059a44ac7ef4e7ec39dc4e0d79ebc036d5a
Nasir <nasirlawal001@gmail.com>
2021-05-11 10:07:52 +0100

    finish positioning component 23 cards

--- src/views/Layout23.vue
+++ src/views/Layout23.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="bg-layout23backgroundColor h-screen">
-    <div class="bg-layout23cardBackgroundColor h-60 rounded-xl mx-auto w-124 top-14 sticky shadow-2xl">
-      first
-    </div>
-    <div class="bg-layout23cardBackgroundColor h-72 rounded-xl mx-auto w-80 mt-28 shadow-2xl">
-      second
+    <div class="flex justify-center">
+      <div class="bg-layout23cardBackgroundColor h-60 rounded-xl w-124 top-6 sticky shadow-2xl">
+        first
+      </div>
+      <div class="bg-layout23cardBackgroundColor h-72 rounded-xl w-80 mt-88 -ml-40 shadow-2xl">
+        second
+      </div>
     </div>
   </div>
 </template>
