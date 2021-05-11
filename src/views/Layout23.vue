841788ab3528d281d4eb7e66c5171753417b2521
Nasir <nasirlawal001@gmail.com>
2021-05-11 10:02:46 +0100

    add the first card in component 23

--- src/views/Layout23.vue
+++ src/views/Layout23.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="bg-layout23backgroundColor h-screen">
-    
+    <div class="bg-layout23cardBackgroundColor h-60 rounded-xl mx-auto w-124 top-14 sticky shadow-2xl">
+
+    </div>
   </div>
 </template>
 
