b3b862a0341f74cbd6f5419206021df2c1d6429f
Nasir <nasirlawal001@gmail.com>
2021-05-11 17:44:04 +0100

    add the chevron and the text

--- src/views/Layout25.vue
+++ src/views/Layout25.vue
@@ -29,7 +29,21 @@
         </div>
       </div>
       <div class="bg-white w-80 h-96 mt-24 mx-10 rounded-xl shadow-2xl">
-        first
+        <div class="pt-8 mx-24 font-semibold text-center text-lg">
+          Jump a specific week
+        </div>
+        <div class="border border-gray-200 mt-4"></div>
+        <div class="flex justify-evenly mt-6 mx-4">
+          <div class="text-base font-semibold w-1/3">
+            <svg class="w-8 h-8 fill-current font-bold mt-0.5 float-left" enable-background="new 0 0 32 32"  id="Слой_1" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path clip-rule="evenodd" d="M11.262,16.714l9.002,8.999  c0.395,0.394,1.035,0.394,1.431,0c0.395-0.394,0.395-1.034,0-1.428L13.407,16l8.287-8.285c0.395-0.394,0.395-1.034,0-1.429  c-0.395-0.394-1.036-0.394-1.431,0l-9.002,8.999C10.872,15.675,10.872,16.325,11.262,16.714z" fill="#121313" fill-rule="evenodd" id="Chevron_Right"/></svg>
+          </div>
+          <div class="text-base font-semibold w-1/3 text-center">
+            Apr 2021
+          </div>
+          <div class="text-base font-semibold w-1/3">
+            <svg class="w-8 h-8 fill-current font-bold mt-0.5 float-right" enable-background="new 0 0 32 32"  id="Слой_1" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path clip-rule="evenodd" d="M21.698,15.286l-9.002-8.999  c-0.395-0.394-1.035-0.394-1.431,0c-0.395,0.394-0.395,1.034,0,1.428L19.553,16l-8.287,8.285c-0.395,0.394-0.395,1.034,0,1.429  c0.395,0.394,1.036,0.394,1.431,0l9.002-8.999C22.088,16.325,22.088,15.675,21.698,15.286z" fill="#121313" fill-rule="evenodd" id="Chevron_Right"/></svg>
+          </div>
+        </div>
       </div>
     </div>
   </div>
