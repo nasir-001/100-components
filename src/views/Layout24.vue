88c2f12b8612b9e9268374a375737749a5f52b82
Nasir <nasirlawal001@gmail.com>
2021-05-11 12:44:36 +0100

    add flex with max of three items

--- src/views/Layout24.vue
+++ src/views/Layout24.vue
@@ -10,8 +10,19 @@
             <path d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path>
           </svg>
         </div>
-      <input placeholder="Search" class="appearance-none block w-full text-gray-700 border border-gray-200 rounded-full py-4 px-4 mb-3 leading-tight pl-10 pr-4">
-    </div>
+        <input placeholder="Search" class="appearance-none block w-full text-gray-700 border border-gray-200 rounded-full py-4 px-4 mb-3 leading-tight pl-10 pr-4">
+      </div>
+      <div class="flex justify-evenly text-center">
+        <div class="text-sm font-semibold w-1/3 text-gray-700">
+          Billing and membership
+        </div>
+        <div class="text-sm font-semibold w-1/3 text-gray-700">
+          Billing and membership
+        </div>
+        <div class="text-sm font-semibold w-1/3 text-gray-700">
+          Billing and membership
+        </div>
+      </div>
     </div>
   </div>
 </template>
