5616e7a2628b6ccfee720b019f4028c799781eea
Nasir <nasirlawal001@gmail.com>
2021-05-15 21:48:10 +0100

    add flex and it's items position

--- src/views/Layout26.vue
+++ src/views/Layout26.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="bg-layout26backgroundColor h-screen">
     <div class="bg-white top-6 sticky w-4/6 mx-auto h-144 rounded-lg shadow-2xl">
-      <div class="flex justify-center mx-10">
-        
+      <div class="flex justify-start mx-10">
+        <div class="mt-4 w-2/5 rounded h-130 bg-gradient-to-tl from-layout26gradientBottomColor to-layout26gradientTopColor">
+          card
+        </div>
+        <div class="w-2/5">
+          
+        </div>
       </div>
     </div>
   </div>
