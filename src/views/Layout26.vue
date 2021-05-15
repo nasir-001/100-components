31f4adfbe038cdd8525348e64b3483aae5d19a87
Nasir <nasirlawal001@gmail.com>
2021-05-15 21:39:46 +0100

    createand positioned the card in component 26

--- src/views/Layout26.vue
+++ src/views/Layout26.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Layout 26
+  <div class="bg-layout26backgroundColor h-screen">
+    <div class="bg-white top-6 sticky w-4/6 mx-auto h-144 rounded-lg shadow-2xl">
+      card
+    </div>
   </div>
 </template>
 
