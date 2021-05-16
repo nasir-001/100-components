8b36a1d5ccd3fd11cf0dcfe0d1a9712cac6a4e75
Nasir <nasirlawal001@gmail.com>
2021-05-16 22:45:53 +0100

    finish the second card

--- src/views/Layout27.vue
+++ src/views/Layout27.vue
@@ -76,6 +76,42 @@
           <div class="border border-layout27textColor mt-8 rounded-md text-white cursor-pointer bg-layout27textColor text-sm text-center font-semibold py-3 mx-6">
             GET STARTED
           </div>
+          <div class="flex justify-start mt-6 mx-6">
+            <div class="text-green-600 w-6 h-6">
+              <svg class="fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-sm font-semibold text-gray-600 ml-1">
+              3 new projects / month
+            </div>
+          </div>
+          <div class="flex justify-start mt-2 mx-6">
+            <div class="text-green-600 w-6 h-6">
+              <svg class="fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-sm font-semibold text-gray-600 ml-1">
+              Basic interaction
+            </div>
+          </div>
+          <div class="flex justify-start mt-2 mx-6">
+            <div class="text-green-600 w-6 h-6">
+              <svg class="fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-sm font-semibold text-gray-600 ml-1">
+              Assets library
+            </div>
+          </div>
         </div>
         <div class="bg-white w-1/3 mt-14 h-114 mx-3 rounded-md shadow-2xl">
           third
