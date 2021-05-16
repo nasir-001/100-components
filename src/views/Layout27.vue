331800cb4e83e936be768ee0b0b72c339ca91c77
Nasir <nasirlawal001@gmail.com>
2021-05-16 22:22:13 +0100

    finish the first card

--- src/views/Layout27.vue
+++ src/views/Layout27.vue
@@ -7,11 +7,48 @@
             Free
           </div>
           <div class="flex justify-center">
-            <div class="text-sm pt-12 text-layout27textColor font-semibold">
+            <div class="text-sm pt-10 text-layout27textColor font-bold">
               $
             </div>
-            <div class="text-4xl pt-12 text-layout27textColor font-bold">
+            <div class="text-4xl pt-10 text-layout27textColor font-bold">
               0
+            </div>
+          </div>
+          <div class="text-center text-layout27textColor pt-2 text-xs font-bold tracking-wider uppercase">
+            Great for starters
+          </div>
+          <div class="text-center text-sm pt-6 font-bold tracking-wider text-layout27textColor mx-10">
+            Discover how to create your first project
+          </div>
+          <div class="border border-layout27textColor mt-8 rounded-md text-layout27textColor text-center font-semibold py-3 mx-6">
+            GET STARTED
+          </div>
+          <div class="flex justify-start mt-6 mx-6">
+            <div class="text-green-600 w-6 h-6">
+              <svg class="fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-sm font-semibold text-layout27textColor ml-1">
+              3 new projects / month
+            </div>
+          </div>
+          <div class="flex justify-start mt-2 ml-8">
+            <div class="text-red-600 w-6 h-6 text-lg">
+              X
+            </div>
+            <div class="text-sm font-semibold text-layout27textColor mt-1 -ml-1">
+              Basic interaction
+            </div>
+          </div>
+          <div class="flex justify-start mt-2 ml-8">
+            <div class="text-red-600 w-6 h-6 text-lg">
+              X
+            </div>
+            <div class="text-sm font-semibold text-layout27textColor mt-1 -ml-1">
+              Assets library
             </div>
           </div>
         </div>
