b5c9379ef3398b9eead4560f41d80df8bb0eaf50
Nasir <nasirlawal001@gmail.com>
2021-05-16 13:49:57 +0100

    add the flex and it's cards with positioning in component 27

--- src/views/Layout27.vue
+++ src/views/Layout27.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="bg-layout27bcakgroundColor h-screen">
-    <div class="top-10 sticky bg-white w-4/6 rounded-lg h-144 mx-auto">
-      card
+    <div class="top-10 sticky bg-layout27cardColor w-4/6 rounded-lg h-144 mx-auto">
+      <div class="flex justify-center mx-10">
+        <div class="bg-white w-1/3 mt-14 h-114 mx-3 rounded-md shadow-2xl">
+          first
+        </div>
+        <div class="bg-white w-1/3 mt-14 h-114 mx-3 rounded-md shadow-2xl">
+          second
+        </div>
+        <div class="bg-white w-1/3 mt-14 h-114 mx-3 rounded-md shadow-2xl">
+          third
+        </div>
+      </div>
     </div>
   </div>
 </template>
