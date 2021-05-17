165e153f6ed925d14c52ceb9913024257b7f1c0f
Nasir <nasirlawal001@gmail.com>
2021-05-17 11:26:28 +0100

    design the card and positioning

--- src/views/Layout28.vue
+++ src/views/Layout28.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    template
+  <div class="bg-gradient-to-tl from-layou28gradientBottomColor to-layout28gradientTopColor h-screen">
+    <div class="top-10 sticky bg-white w-72 rounded-lg h-4/5 mx-auto">
+      card
+    </div>
   </div>
 </template>
 
