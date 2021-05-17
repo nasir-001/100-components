01252ed2531b64f29c827e9cf6105cc9465bdc3e
Nasir <nasirlawal001@gmail.com>
2021-05-17 11:45:34 +0100

    desingning the component 28

--- src/views/Layout28.vue
+++ src/views/Layout28.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="bg-gradient-to-tl from-layou28gradientBottomColor to-layout28gradientTopColor h-screen">
     <div class="top-10 sticky bg-white w-72 rounded-lg h-4/5 mx-auto">
-      card
+      <div class="bg-layou28smallCircleColor mt-2 mr-2 float-right w-8 h-8 rounded-full text-center text-lg text-gray-500">
+        X
+      </div>
+      <div class="ml-5 mr-20 text-lg font-semibold pt-5 text-gray-700">
+        Create an account to view this page
+      </div>
+      <div class="text-xs ml-5 pt-3 font-semibold text-gray-700">
+        Already have an account? <i class="text-blue-600 underline">Login</i>
+      </div>
     </div>
   </div>
 </template>
