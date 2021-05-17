d1f2b046896b44b65943e86f30f5a9b9d7c55907
Nasir <nasirlawal001@gmail.com>
2021-05-17 14:02:22 +0100

    add facebook svg to twitter button

--- src/views/Layout28.vue
+++ src/views/Layout28.vue
@@ -48,7 +48,7 @@
           <svg class="w-6 h-5 mx-auto text-white fill-current" xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
         </div>
         <div class="bg-layout28twitterButtonColor w-1/2 cursor-pointer shadow-xl ml-2 py-3 rounded-md">
-          face
+          <svg class="w-6 h-5 mx-auto text-white fill-current" xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
         </div>
       </div>
     </div>
