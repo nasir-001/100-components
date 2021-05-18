138f5ab8f32d92a9baadc12e37058608f949b5e8
Nasir <nasirlawal001@gmail.com>
2021-05-18 23:05:19 +0100

    add flex and it's cards

--- src/views/Layout30.vue
+++ src/views/Layout30.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="bg-layout30backgroundColor h-screen">
     <div class="bg-layout30cardColor top-10 sticky rounded-md w-4/6 h-5/6 mx-auto">
-
+      <div class="flex justify-evenly mx-12">
+        <div class="w-2/3">
+          double
+        </div>
+        <div class="w-1/3">
+          single
+        </div>
+      </div>
     </div>
   </div>
 </template>
