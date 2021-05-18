6bf06334b6514b1d447ed6df3d57395ca9417111
Nasir <nasirlawal001@gmail.com>
2021-05-18 22:57:15 +0100

    design and postioned the card to the component 30

--- src/views/Layout30.vue
+++ src/views/Layout30.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="bg-layout30backgroundColor h-screen">
-    tem
+    <div class="bg-layout30cardColor top-10 sticky rounded-md w-4/6 h-5/6 mx-auto">
+
+    </div>
   </div>
 </template>
 <script>
