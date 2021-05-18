9a3603ff512170dc07c41457cc2ed062c9c9be69
Nasir <nasirlawal001@gmail.com>
2021-05-18 20:53:08 +0100

    designing the component

--- src/views/Layout29.vue
+++ src/views/Layout29.vue
@@ -3,7 +3,24 @@
     <div class="bg-white my-10 sticky w-4/6 rounded-md h-164 mx-auto">
       <div class="flex mx-12">
         <div class="w-1/3 h-52 bg-layout29firstCardColor mt-14 mx-2">
-          card
+          <div class="pt-5 pl-5 text-gray-300 text-xs font-bold">
+            MAY
+          </div>
+          <div class="text-2xl font-bold pl-5 text-gray-100">
+            4
+          </div>
+          <div class="pt-3 px-5 text-sm text-gray-300 font-bold tracking-widest">
+            Computer Hardware Desktops And Notebooks And Handhelds Oh My
+          </div>
+          <div class="flex justify-between mx-5">
+            <div class="text-gray-300 text-xs pt-4 font-bold">
+              9:00 - 11:00
+              <div class="text-gray-300 text-xs">
+                Online
+              </div>
+            </div>
+            <div class="mt-5 text-gray-300"></div>
+          </div>
         </div>
         <div class="w-1/3 h-52 bg-layout29secondCardColor mt-14 mx-2">
           card
