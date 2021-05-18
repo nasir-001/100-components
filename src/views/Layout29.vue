fd180aa0e87ac99a45162052466db1da2f383e87
Nasir <nasirlawal001@gmail.com>
2021-05-18 22:15:45 +0100

    finishing component 29

--- src/views/Layout29.vue
+++ src/views/Layout29.vue
@@ -82,7 +82,7 @@
           </div>
           <div class="flex justify-between mx-5">
             <div class="text-gray-600 text-xs pt-4 font-bold">
-              12:00 - 14:00
+              9:00 - 11:00
               <div class="text-gray-600 text-xs">
                 Online
               </div>
@@ -93,10 +93,48 @@
           </div>
         </div>
         <div class="w-1/3 h-52 bg-layout29firstCardColor mt-4 mx-2">
-          card
+          <div class="pt-5 pl-5 text-gray-300 text-xs font-bold">
+            MAY
+          </div>
+          <div class="text-2xl font-bold pl-5 text-gray-100">
+            4
+          </div>
+          <div class="pt-3 px-5 text-sm text-gray-300 font-bold tracking-wider">
+            Help Finding Information Online
+          </div>
+          <div class="flex justify-between mx-5">
+            <div class="text-gray-300 text-xs pt-8 font-bold">
+              11:00 - 13:00
+              <div class="text-gray-300 text-xs">
+                Online
+              </div>
+            </div>
+            <div class="mt-10 text-gray-300">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 32 32"><path clip-rule="evenodd" d="M32,16.009c0-0.267-0.11-0.522-0.293-0.714  l-9.899-9.999c-0.391-0.395-1.024-0.394-1.414,0c-0.391,0.394-0.391,1.034,0,1.428l8.193,8.275H1c-0.552,0-1,0.452-1,1.01  s0.448,1.01,1,1.01h27.586l-8.192,8.275c-0.391,0.394-0.39,1.034,0,1.428c0.391,0.394,1.024,0.394,1.414,0l9.899-9.999  C31.894,16.534,31.997,16.274,32,16.009z" fill-rule="evenodd" id="Arrow_Forward"/></svg>
+            </div>
+          </div>
         </div>
         <div class="w-1/3 h-52 bg-layout29secondCardColor mt-4 mx-2">
-          card
+          <div class="pt-5 pl-5 text-layout29textColor text-xs font-bold">
+            MAY
+          </div>
+          <div class="text-2xl font-bold pl-5 text-layout29textColor">
+            18 - 20
+          </div>
+          <div class="pt-3 px-5 text-sm text-gray-700 font-bold tracking-wider">
+            Steps In Installing Rack Mount Lcd Monitors
+          </div>
+          <div class="flex justify-between mx-5">
+            <div class="text-gray-600 text-xs pt-8 font-bold">
+              13:00 - 14:00
+              <div class="text-gray-600 text-xs">
+                Online
+              </div>
+            </div>
+            <div class="mt-10 text-layout29textColor">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 32 32"><path clip-rule="evenodd" d="M32,16.009c0-0.267-0.11-0.522-0.293-0.714  l-9.899-9.999c-0.391-0.395-1.024-0.394-1.414,0c-0.391,0.394-0.391,1.034,0,1.428l8.193,8.275H1c-0.552,0-1,0.452-1,1.01  s0.448,1.01,1,1.01h27.586l-8.192,8.275c-0.391,0.394-0.39,1.034,0,1.428c0.391,0.394,1.024,0.394,1.414,0l9.899-9.999  C31.894,16.534,31.997,16.274,32,16.009z" fill-rule="evenodd" id="Arrow_Forward"/></svg>
+            </div>
+          </div>
         </div>
       </div>
       <div class="bg-layout29buttonColor mx-14 mt-6 text-center uppercase text-xs font-bold py-8 text-gray-200 cursor-pointer">
