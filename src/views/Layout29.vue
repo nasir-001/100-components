1d347a10a80f4d48a2309c0ccb028d77a76bb6b3
Nasir <nasirlawal001@gmail.com>
2021-05-18 19:40:47 +0100

    add the six cards to the component 29

--- src/views/Layout29.vue
+++ src/views/Layout29.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="bg-layout29backgroundColor h-screen">
     <div class="bg-white top-10 sticky w-4/6 rounded-md h-5/6 mx-auto">
-      card
+      <div class="flex mx-12">
+        <div class="w-1/3 h-52 bg-layout29firstCardColor mt-14 mx-2">
+          card
+        </div>
+        <div class="w-1/3 h-52 bg-layout29secondCardColor mt-14 mx-2">
+          card
+        </div>
+        <div class="w-1/3 h-52 bg-layout29firstCardColor mt-14 mx-2">
+          card
+        </div>
+      </div>
+      <div class="flex mx-12">
+        <div class="w-1/3 h-52 bg-layout29secondCardColor mt-4 mx-2">
+          card
+        </div>
+        <div class="w-1/3 h-52 bg-layout29firstCardColor mt-4 mx-2">
+          card
+        </div>
+        <div class="w-1/3 h-52 bg-layout29secondCardColor mt-4 mx-2">
+          card
+        </div>
+      </div>
     </div>
   </div>
 </template>
