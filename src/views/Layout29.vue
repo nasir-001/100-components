8cc7dd28c9a7a6c577322e08cabf4904796e317a
Nasir <nasirlawal001@gmail.com>
2021-05-18 19:50:21 +0100

    finishing the button of component 29

--- src/views/Layout29.vue
+++ src/views/Layout29.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-layout29backgroundColor h-screen">
-    <div class="bg-white top-4 sticky w-4/6 rounded-md h-164 mx-auto">
+  <div>
+    <div class="bg-white my-10 sticky w-4/6 rounded-md h-164 mx-auto">
       <div class="flex mx-12">
         <div class="w-1/3 h-52 bg-layout29firstCardColor mt-14 mx-2">
           card
@@ -22,6 +22,9 @@
         <div class="w-1/3 h-52 bg-layout29secondCardColor mt-4 mx-2">
           card
         </div>
+      </div>
+      <div class="bg-layout29buttonColor mx-14 mt-6 text-center uppercase text-xs font-bold py-8 text-gray-200 cursor-pointer">
+        Learn more events
       </div>
     </div>
   </div>
