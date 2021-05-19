455cb8fba31e5449b0effaa81cfc0d4bca42614a
Nasir <nasirlawal001@gmail.com>
2021-05-19 11:59:33 +0100

    add all the mark svg and the text in the right side of the component

--- src/views/Layout30.vue
+++ src/views/Layout30.vue
@@ -21,6 +21,54 @@
               Experienced mentors
             </div>
           </div>
+          <div class="flex justify-start mx-4 mt-2">
+            <div class="text-gray-800">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-gray-700 text-xs font-semibold mt-1 ml-1">
+              Proven track record
+            </div>
+          </div>
+          <div class="flex justify-start mx-4 mt-2">
+            <div class="text-gray-800">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-gray-700 text-xs font-semibold mt-1 ml-1">
+              Effective process
+            </div>
+          </div>
+          <div class="flex justify-start mx-4 mt-2">
+            <div class="text-gray-800">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-gray-700 text-xs font-semibold mt-1 ml-1">
+              Strong industry network
+            </div>
+          </div>
+          <div class="flex justify-start mx-4 mt-2">
+            <div class="text-gray-800">
+              <svg class="w-6 h-6 fill-current" viewBox="0 0 50 50" enable-background="new 0 0 50 50" xml:space="preserve">
+              <path d="M40.267,14.628L20.974,33.921l-9.293-9.293c-0.391-0.391-1.023-0.391-1.414,0s-0.391,1.023,0,1.414l10,10
+                c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293l20-20c0.391-0.391,0.391-1.023,0-1.414S40.657,14.237,40.267,14.628z
+                "/>
+              </svg>
+            </div>
+            <div class="text-gray-700 text-xs font-semibold mt-1 ml-1">
+              Relavant courses
+            </div>
+          </div>
         </div>
       </div>
     </div>
